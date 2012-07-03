c61611d5216a62a176027eec33cdd6b0c1983df7
xandy <gitmail.xandy@xoxy.net>
2012-07-03 22:28:59 +0200

    updated readme

---
+++ README.mk
@@ -0,0 +1,11 @@
+# Commands
+* `!ping` 
+* `!np` current song
+* `!status` room status
+* `!help` helptext
+* `!add <streamurl>` add url to playlist
+* `!play <streamurl>` play url
+
+# Required packages
+* irc
+* mpdsocket
