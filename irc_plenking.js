8cdf271f41c794208fba01d9901b1124be1c667c
xandy <gitmail.xandy@xoxy.net>
2012-09-22 06:02:20 +0200

    update plenking module to module template structure

--- irc_plenking.js
+++ irc_plenking.js
@@ -1,32 +1,39 @@
+var FILTERS = {};
+var COMMANDS = {};
+var LOGGER;
+var CONFIG;
+
+var MODULE_NAME = "PLENKING";
+
 var plenkers={};
-var logger;
-var config;
 var plenkingWait    = 30*60*1000;//30min
-var filters = {
-    plenking :  function(message, sender, to){
-                    var expr = /\s{2,}/g ;
-                    var hits = message.match(expr);
-                    if( hits && (hits.length>=2) ){
-                        logger.info("plenking detected user: " + sender);
-                        if(plenkers[sender]){
-                            logger.warn("kicking user: " + sender + " from channel " + to);
-                            this.rc_client.send("kick", to, sender, "plenking");
-                        }else{
-                            this.reply(sender, to, "bitte hier kein plenking.");
-                            plenkers[sender] = true;
-                            setTimeout(function(){
-                                plenkers[sender] = undefined;
-                                logger.info("plenking cleared for user " + sender);
-                            },plenkingWait);
-                        }  
-                    }
-                }, 
-    };
+
+FILTERS.plenking = function(message, sender, to){
+    var expr = /\s{2,}/g ;
+    var hits = message.match(expr);
+    if( hits && (hits.length>=2) ){
+        LOGGER.info("plenking detected user: " + sender);
+        if(plenkers[sender]){
+            LOGGER.warn("kicking user: " + sender + " from channel " + to);
+            this.irc_client.send("kick", to, sender, "plenking");
+        }else{
+            this.reply(sender, to, "bitte hier kein plenking.");
+            plenkers[sender] = true;
+            setTimeout(function(){
+                plenkers[sender] = undefined;
+                LOGGER.info("plenking cleared for user " + sender);
+            },plenkingWait);
+        }  
+    }
+};
 
 module.exports = function(cfg, log, bot){
-    logger = log.getLogger("plenking");
-    config = cfg;
-    for(key in filters){
-        bot.filters[key] = filters[key];
+    LOGGER = log.getLogger(MODULE_NAME);
+    CONFIG = cfg;
+    for(key in COMMANDS){
+        bot.commands[key] = COMMANDS[key];
+    }
+    for(key in FILTERS){
+        bot.filters[key] = FILTERS[key];
     }
 };
