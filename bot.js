6e72022c8e86bf53da116ca7c3e6cf93c72c02c5
xandy <gitmail.xandy@xoxy.net>
2012-09-22 05:34:25 +0200

    more refactoring

--- bot.js
+++ bot.js
@@ -10,18 +10,16 @@ try{
 }catch(e){
 	config = require('./config.js');
 }
-var nick = config.nick;
-var realname = config.realname;
-var username = config.username;
-var irc_server = config.irc_server;
-var irc_port = config.irc_port;
-var ircpass = config.ircpass;
-var secure = config.secure;
-var ignoreSsl = config.ignoreSsl;
-var channels = config.channels;
+var nick        = config.nick;
+var realname    = config.realname;
+var username    = config.username;
+var irc_server  = config.irc_server;
+var irc_port    = config.irc_port;
+var ircpass     = config.ircpass;
+var secure      = config.secure;
+var ignoreSsl   = config.ignoreSsl;
+var channels    = config.channels;
 var disable_mpd = config.disable_mpd;
-
-var score_cooldown  = 2000;
 
 
 /*REQUIRES*/
@@ -37,54 +35,45 @@ log4js.configure({
     ]
 });
 
-//var l_mpd   = log4js.getLogger("mpd");
-var l_irc   = log4js.getLogger("irc");
-var l_karma = log4js.getLogger("karma");
-var l_blacklist = log4js.getLogger("blacklist");
-var l_plenking = log4js.getLogger("plenking");
-var l_webrelais = log4js.getLogger("webrelais");
+
 var l_other = log4js.getLogger("other");
 l_other.info("STARTUP");
 
 
 /*IRC SETUP*/
-var ircclient   = new irc.Client( irc_server, nick, {
-    'channels'      : channels,
-    'userName'      : username,
-    'realName'      : realname,
-    'port'          : irc_port,
-    'secure'        : secure,
-    'selfSigned'    : ignoreSsl,
-    'certExpired'   : ignoreSsl,
-    'password'      : ircpass,
-});
-
-
-
-ircclient.addListener('message', function (from, to, message) {
-    if(isChannel(from, to) && (from!=nick) ){
-        contentFilter(message, from, to)
-    }
-
-    if(from!=nick){
-        messageDispatcher(message, from, to);
-    }
-});
-ircclient.addListener('error', function(message){
-    l_irc.error(message);
-});
 
 
 /* SPACE API SETUP*/
 
 var IrcBot = function(){
+    var that = this;
+    this.irc_client = new irc.Client( irc_server, nick, {
+        'channels'      : channels,
+        'userName'      : username,
+        'realName'      : realname,
+        'port'          : irc_port,
+        'secure'        : secure,
+        'selfSigned'    : ignoreSsl,
+        'certExpired'   : ignoreSsl,
+        'password'      : ircpass,
+    });
+    this.irc_client .addListener('message', function (from, to, message) {
+        if((from!=nick) && (!that.contentFilter(message, from, to)) ){
+            that.messageDispatcher(message, from, to);
+        }
+    });
+    this.irc_client .addListener('error', function(message){
+        l_other.error(message);
+    });
     this.commands={}; 
     this.filters={};
-    this.irc_client = ircclient;
+    //this.irc_client = ircclient;
     require('./irc_core.js')(config, log4js, this);
     require('./irc_plenking.js')(config, log4js, this);
     require('./irc_karma.js')(config, log4js, this);
     require('./irc_bckspc.js')(config, log4js, this);
+    if(!disable_mpd)
+        require('./irc_mpd.js')(config, log4js, this);
 };
 
 IrcBot.prototype.sendToWho = function(sender, to){
@@ -98,91 +87,22 @@ IrcBot.prototype.isChannel = function(sender, to){
 IrcBot.prototype.reply = function(sender, to, message){
     if(this.isChannel(sender, to))
         message = sender + " " + message;
-    this.irc_client.say(sendToWho(sender, to), message);
+    this.irc_client.say(this.sendToWho(sender, to), message);
 };
 
-var bot = new IrcBot();
-
-
-if(!disable_mpd)
-    require('./irc_mpd.js')(config, log4js, bot);
-
-/*code*/
-
-
-var messageDispatcher = function(message, sender, to){
+IrcBot.prototype.messageDispatcher = function(message, sender, to){
     var args    = message.split(' ');
     var command = args[0];
     var fun;
-    if(fun = commands[command]){
-        if(dropMessage(message, sender, to)){
-            if (getBlCount(sender) == 5){
-                reply(sender, to, "Wat R U Doin");
-                reply(sender, to, "Stahp!");
-            }
-            return;
-        }
-            
-        fun.apply(bot, [sender, to].concat(args.slice(1)) );
+    if(fun = this.commands[command]) 
+        fun.apply(this, [sender, to].concat(args.slice(1)) );
+};
+
+IrcBot.prototype.contentFilter = function(message, sender, channel){
+    for(var name in this.filters){
+        if( this.filters[name].apply(bot, [message, sender, channel]))
+            return true;
     }
 };
 
-var contentFilter = function(message, sender, channel){
-    for(var name in Filters){
-        Filters[name].apply(bot, [message, sender, channel]);
-    }
-};
-
-
-var bl_scores = {};
-var dropMessage = function(message, sender, channel){
-    var score = bl_scores[sender]||0;
-    var newscore = score+1;
-    bl_scores[sender] = newscore;
-    l_blacklist.debug("incr score "+sender+ " "+ bl_scores[sender]);
-    setTimeout(function(){
-        if(bl_scores[sender]==newscore){//no incement afert this one
-            bl_scores[sender]=0;
-            l_blacklist.debug("cooldown "+sender+ " "+ bl_scores[sender]);
-        }else{
-            l_blacklist.debug("no cooldown "+sender+ " "+ bl_scores[sender] + " != " + newscore);
-        }
-
-    }, newscore*score_cooldown);
-    if(score>0)
-        l_blacklist.warn("dropped user " + sender);
-    return score>0;
-};
-var getBlCount = function(sender){
-    return bl_scores[sender];
-};
-
-var sendToWho = function(sender, to){
-    return isChannel(sender, to) ? to : sender;
-};
-
-var isChannel = function(sender, to){
-    return to && to[0]=='#';
-};
-
-var reply = function(sender, to, message){
-    if(isChannel(sender, to))
-        message = sender + " " + message;
-    ircclient.say(sendToWho(sender, to), message);
-};
-
-
-
-var ircColors = {
-    red : function(text){
-        return irc.colors.wrap('dark_red', text);
-    },
-    green : function(text){
-        return irc.colors.wrap('light_green', text);
-    },
-};
-
-
-
-var commands = bot.commands;
-var Filters = bot.filters;
+var bot = new IrcBot();
--- irc_core.js
+++ irc_core.js
@@ -5,6 +5,8 @@ var commands = {};
 var logger;
 var running_version = "unknown";
 
+var score_cooldown = 2 * 1000;
+
 
 (commands['!update'] = function(sender, to){
     var cmd = 'git pull origin master';
@@ -52,10 +54,48 @@ var running_version = "unknown";
     '!help' :   'see https://github.com/b4ckspace/ircbot',
 */
 
+var bl_scores = {};
+var dropMessage = function(message, sender, to){
+    var score = bl_scores[sender]||0;
+    var newscore = score+1;
+    bl_scores[sender] = newscore;
+    logger.debug("incr score "+sender+ " "+ bl_scores[sender]);
+    setTimeout(function(){
+        if(bl_scores[sender]==newscore){//no incement afert this one
+            bl_scores[sender]=0;
+            logger.debug("cooldown "+sender+ " "+ bl_scores[sender]);
+        }else{
+            logger.debug("no cooldown "+sender+ " "+ bl_scores[sender] + " != " + newscore);
+        }
+
+    }, newscore*score_cooldown);
+    if(score>0)
+        logger.warn("dropped user " + sender);
+    return score>0;
+};
+var getBlCount = function(sender){
+    return bl_scores[sender];
+};
+
+var filters = {
+    flood : function(message, sender, to){
+        if(dropMessage(message, sender, to)){
+            if(getBlCount(sender)==5){
+                this.reply(sender, to, "Wat R U Doin");
+                this.reply(sender, to, "Stahp!");
+            }
+            return true
+        }
+    },
+};
+
 module.exports = function(cfg, log, bot){
     logger = log.getLogger("core");
     for(key in commands){
         bot.commands[key] = commands[key];
+    }
+    for(key in filters){
+        bot.filters[key] = filters[key];
     }
     exec('git log -n 1 HEAD --format=oneline', function (e, stdout, stderr) {
         if (e !== null) {
