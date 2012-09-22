05ca60750c747c2df6747fcb908c2d0866c5f2fb
xandy <gitmail.xandy@xoxy.net>
2012-09-22 05:40:58 +0200

    add module template

---
+++ irc_module.template.js
@@ -0,0 +1,19 @@
+var FILTERS = {};
+var COMMANDS = {};
+var LOGGER;
+var CONFIG;
+
+var MODULE_NAME = "your name here";
+
+/*YOUR CODE HERE*/
+
+module.exports = function(cfg, log, bot){
+    LOGGER = log.getLogger(MODULE_NAME);
+    for(key in COMMANDS){
+        bot.commands[key] = COMMANDS[key];
+    }
+    for(key in FILTERS){
+        bot.filters[key] = FILTERS[key];
+    }
+    /*YOUR INIT CODE HERE*/
+};
