3d05cdfcdd8ace4529f9058d877ef374ce30f09f
xandy <gitmail.xandy@xoxy.net>
2012-09-22 06:32:21 +0200

    match bckspcmodule to module template

--- irc_bckspc.js
+++ irc_bckspc.js
@@ -1,36 +1,20 @@
+var FILTERS = {};
+var COMMANDS = {};
+var LOGGER;
+var CONFIG;
+
+var MODULE_NAME = "your name here";
+
 var bckspcApi   = require('./bckspcapi.js');
 var webrelaisApi= require('./webrelais.js');
-var webrelais = new webrelaisApi.Client("https://webrelais.bckspc.de:443");
-var logger;
-var config;
-var spaceApi = new bckspcApi();
-var alarmWait       = 60*1000;
-
-var topics      = {};
+var webrelais   = new webrelaisApi.Client("https://webrelais.bckspc.de:443");
+var spaceApi    = new bckspcApi();
+var alarmWait   = 60*1000;
 
 
-var setTopic = function(channel, connection){
-    if(!spaceApi.isReady()){
-        setTimeout(function(){setTopic(channel, connection)}, 500);
-        return;
-    }
-    if(spaceApi.isOpen()){
-        message = "open";
-    }else{
-        message = "closed";
-    }
-    var topicExpr=/open|closed/g;
-    if(!topics[channel])
-        return;
-    var newTopic = topics[channel].replace(topicExpr, message);
-    if(newTopic != topics[channel]){
-        connection.send("topic", channel, newTopic);
-    }
-};
 
 
-var commands = {};
-(commands['!status'] = function(sender, to){
+(COMMANDS['!status'] = function(sender, to){
     var message;
     if(spaceApi.isOpen()){
         message = "open (" + spaceApi.openCount() + ")";
@@ -43,21 +27,21 @@ var commands = {};
 
 
 var alarm_blocked = false;
-(commands['!alarm'] = function(sender, to) {
+(COMMANDS['!alarm'] = function(sender, to) {
     if(!this.isChannel(sender, to)){
         this.reply(sender, to, "you can use !alarm only in channels.");
-        logger.info("alarm not in channel %s", sender);
+        LOGGER.info("alarm not in channel %s", sender);
         return;
     }
     if(alarm_blocked){
         this.reply(sender, to, "https://www.youtube.com/watch?v=TqDsMEOYA9g");
-        logger.info("alarm not ready %s %s", sender, to);
+        LOGGER.info("alarm not ready %s %s", sender, to);
         return;
     }
     alarm_blocked=true;
     setTimeout(function(){
         alarm_blocked = false;
-        logger.debug("alarm cooldown");
+        LOGGER.debug("alarm cooldown");
     }, alarmWait);
     var white = 3;
     var red   = 4;
@@ -65,7 +49,7 @@ var alarm_blocked = false;
     var off   = 0;
     var waittime = 500;
     this.reply(sender, to, "alarm has been activated. backspace is now in defcon 2.");
-    logger.info("alarm in channel %s by user %s", to, sender);
+    LOGGER.info("alarm in channel %s by user %s", to, sender);
     webrelais.set_port(white, on, function(){
         setTimeout(function(){
             webrelais.set_port(white, off, function(){});
@@ -83,27 +67,53 @@ var alarm_blocked = false;
     });
 }).helptext = "flash the emergency light :)";
 
+(COMMANDS['!pampus'] = function(sender, to, command){
+    this.reply(sender, to, 'Dem Pampus fehlt Salz!');
+}).helptext = 'Dem Pampus fehlt Salz!';
 
-/*
-    '!pampus':  'Dem Pampus fehlt Salz!',
-    '!nerf':    'phew! phew! nerfgunfight!',
-*/
+(COMMANDS['!nerf'] = function(sender, to, command){
+    this.reply(sender, to, 'phew! phew! nerfgunfight!');
+}).helptext = 'phew! phew! nerfgunfight!';
 
 
 module.exports = function(cfg, log, bot){
-    logger = log.getLogger("bckspc");
-    config = cfg;
-    for(key in commands){
-        bot.commands[key] = commands[key];
+    LOGGER = log.getLogger(MODULE_NAME);
+    CONFIG = cfg;
+    for(key in COMMANDS){
+        bot.commands[key] = COMMANDS[key];
+    }
+    for(key in FILTERS){
+        bot.filters[key] = FILTERS[key];
     }
     bot.irc_client.addListener('topic', function (channel, topic, nick, message){
         topics[channel] = topic;
         setTopic(channel, bot.irc_client);
-        logger.debug("set channel, topic: " + channel + " , " + topic);
+        LOGGER.debug("set channel, topic: " + channel + " , " + topic);
     });
     spaceApi.on('isopen', function(open){
-    for(var k in channels){
-        setTopic(k, bot.irc_client);
+        for(var k in channels){
+            setTopic(k, bot.irc_client);
+        }
+    });
+};
+
+
+var topics      = {};
+var setTopic = function(channel, connection){
+    if(!spaceApi.isReady()){
+        setTimeout(function(){setTopic(channel, connection)}, 500);
+        return;
     }
-});
+    if(spaceApi.isOpen()){
+        message = "open";
+    }else{
+        message = "closed";
+    }
+    var topicExpr=/open|closed/g;
+    if(!topics[channel])
+        return;
+    var newTopic = topics[channel].replace(topicExpr, message);
+    if(newTopic != topics[channel]){
+        connection.send("topic", channel, newTopic);
+    }
 };
