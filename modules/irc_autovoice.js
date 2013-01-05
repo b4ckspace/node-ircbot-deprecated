d98f0d1f5b956415450ea1026db2477d6fb853b2
schinken <schinken@hackerspace-bamberg.de>
2013-01-05 16:49:10 +0100

    refactoring

--- modules/irc_autovoice.js
+++ modules/irc_autovoice.js
@@ -7,11 +7,15 @@ var MODULE_NAME = "AUTOVOICE";
 var util = require("util");
 var bot_;
 
+// Normalize nickname for better matching
 function normalizeNick(nick) {
     return nick.replace(/[^a-zA-Z]+/, '').toLowerCase();    
 }
 
-var inSpace = function(username){
+function inSpace(username){
+
+    // Checks if the username is currently in space, and exits
+    // the loop if so
     return bot_._bckspcapi.getMembers().some(function(value, key) {
         if(normalizeNick(value) == normalizeNick(username)) {
             return true;
@@ -19,64 +23,85 @@ var inSpace = function(username){
     });
 };
 
-var setAllChans = function(){
-    for(channel in bot_.topics){
-        setVoices(channel);
-    }
-};
+function setAllChans() {
+    bot_.topics.forEach(function(v, channel) {
+        setVoices(channel);    
+    });    
+}
 
 var setVoices = function(channel){
+
     bot_.irc_client.once('names', function(chname, names){
-        if(channel!=chname){
+
+        if(channel != chname){
             LOGGER.debug("setVoice channel!=nchannel %s != %s", channel, chname);
             return;
         }
+
         var voicepeople = {};
-        for(key in names){
-            voicepeople[key.toLowerCase()]=(names[key].indexOf("v")!=-1);
-        }
-        for(username in voicepeople){
-            if(voicepeople[username] && !inSpace(username)){
+
+        // Iterate all names inside the channel and check if the nick is voiced or not
+        names.forEach(function(modes, username) {
+
+            var is_voiced = (modes.indexOf("v") != -1);
+            var in_space = inSpace(username);
+            
+            // Check if user is voiced but not in space anymore
+            if(is_voiced && !in_space) {
                 LOGGER.debug("user has voice, but not in space: %s", username);
                 bot_.irc_client.send('mode', channel, '-v', username);
             }
-            if((!voicepeople[username]) && inSpace(username)){
+
+            // Check if user is not voiced, but in space
+            if(!is_voiced && in_space) {
                 LOGGER.debug("user has no voice, but in space: %s", username);
                 bot_.irc_client.send('mode', channel, '+v', username);
             }
-        }
+        });
+
     });
+
     bot_.irc_client.send('names', channel);
 };
 
 module.exports = function(cfg, log, bot){
+
     LOGGER = log.getLogger(MODULE_NAME);
     CONFIG = cfg;
     bot_   = bot;
+
     bot.irc_client.on('join', function(channel, nick, message){
         LOGGER.debug("user %s joined chanel", nick, channel);
         setVoices(channel);
     });
+
     bot._bckspcapi.on('join', function(member){
         LOGGER.debug('member join: %s', member);
         setAllChans();
     });
+
     bot._bckspcapi.on('part', function(member){
         LOGGER.debug('member part: %s', member);
         setAllChans();
     });
+
     bot.irc_client.on('+mode', function(channel, by, mode, arg, msg){
         if(by==bot.nick)
             return;
         LOGGER.debug('+modechange: %s, %s, %s, %s, %s', channel, by, mode, arg, msg);
         setVoices(channel);
     });
+
     bot.irc_client.on('-mode', function(channel, by, mode, arg, msg){
-        if(by==bot.nick)
+        
+        if(by == bot.nick) {
             return;
+        }
+
         LOGGER.debug('-modechange: %s, %s, %s, %s, %s', channel, by, mode, arg, msg);
         setVoices(channel);
     });
+
     bot.irc_client.on('nick', function(oldnick, newnick, channels){
         LOGGER.debug('nickchange: %s -> %s', oldnick, newnick);
         for(i in channels){
