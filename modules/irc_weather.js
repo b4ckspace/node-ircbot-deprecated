fc470aeedb685ce7364095a810010549aa692f3a
xandy <gitmail.xandy@xoxy.net>
2013-02-28 15:27:51 +0100

    optional town & country parameters for weather module

--- modules/irc_weather.js
+++ modules/irc_weather.js
@@ -15,13 +15,22 @@ var currentweather = undefined;
 var fetchTimeout = 5 * 60 * 1000;
 
 var updateWeather = function(){
-    var url = weatherbase + town;
-    LOGGER.debug("fetch new data, url: %s", url);
+    fetchWeatherByName("Bamberg", "Germany", function(res){
+        currentweather=res;
+    });
+    setTimeout(updateWeather, fetchTimeout);
+};
+
+var fetchWeatherByName = function(town, country, callback){
+    if(!town)
+        town="Bamberg";
+    if(!country)
+        country="Germany"
+    var url = weatherbase + town + "," + country;
+    LOGGER.debug("fetch new data. url: %s", url);
     http.get(url, function(res){
         var resString = '';
-
         res.setEncoding('utf8');
-
         res.on( 'data', function( data ) {
             resString += data;
         } );
@@ -33,23 +42,51 @@ var updateWeather = function(){
                 LOGGER.error("json parse error: " + e.message);
                 return;
             }
+            if(status.message){
+                LOGGER.warn("api error: %s for town, country: %s, %s", status.message, town, country);
+                callback('error: "' + status.message + '"');
+                return;
+            }
             var temp = status.list[0].main.temp - 273.15; //temp is in kelvin
             var weather = status.list[0].weather[0].main;
-            currentweather = sprintf("%s (%.2f°C)", weather, temp);
+            var link = status.list[0].url;
+            var realtown = status.list[0].name;
+            var realcountry = status.list[0].sys.country;
+            var ret = sprintf("weather for %s, %s: %s (%.2f°C) more: %s",realtown, realcountry, weather, temp, link);
+            callback(ret);
         });
     }).on('error', function(e) {
         LOGGER.error("Got http status error error: " + e.message);
     });
-    setTimeout(updateWeather, fetchTimeout);
 };
 
 
-(COMMANDS['!weather'] = function(sender, to, user){
-    if(currentweather){
-        this.reply(sender, to, currentweather);
-        return;
+(COMMANDS['!weather'] = function(sender, to){
+    var term = Array.prototype.slice.call(arguments, 2).join(' ');
+    if(!term){
+        if(currentweather){
+            this.reply(sender, to, currentweather);
+        }else{
+            this.reply(sender, to, "no weather data fetched yet.");
+        }
+        return
     }
-    this.reply(sender, to, "no weather data fetched yet.");
+    var matches = term.split(',');
+    var town = matches[0];
+    var country=matches[1];
+    if(!country){
+        country="Germany";
+    }else{
+        country = country.match(/\w+[\s\w+]*\w+/)[0];
+        if(!country)
+            country="Germany";
+    }
+    town=town.match(/\w+[\s\w+]*\w+/)[0];
+    var that = this;
+    fetchWeatherByName(town, country, function(res){
+        that.reply(sender, to, res);
+    })
+
 }).helptext = "get current weather data.";
 
 COMMANDS['!wetter'] = COMMANDS['!weather'];
